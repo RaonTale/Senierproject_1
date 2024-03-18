5b3e5d4aadec1dcb99fd70c0e31268a7955f90e9
RaonTale <wngusvlf@gmail.com>
2024-03-18 18:58:33 +0900

    first commit

---
+++ Source/Senierproject_1/Senierproject_1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Senierproject_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Senierproject_1, "Senierproject_1" );
---
+++ Source/Senierproject_1/Senierproject_1GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "Senierproject_1GameModeBase.h"
+
